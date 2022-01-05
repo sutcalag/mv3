c57dd7daeb94d7c23f55a6fb1afc82117daeda61
ThyeeZz <jialian.ji@zilliz.com>
2022-01-05 21:32:35 +0800

    complete demo page

--- src/components/snackBar/index.jsx
+++ src/components/snackBar/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Snackbar, MuiAlert } from '@mui/material';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
