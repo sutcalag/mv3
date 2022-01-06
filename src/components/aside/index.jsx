47e188af541c89a7dd33f267e3149220cff3f43b
ThyeeZz <jialian.ji@zilliz.com>
2022-01-06 16:58:45 +0800

    stash

--- src/components/aside/index.jsx
+++ src/components/aside/index.jsx
@@ -3,6 +3,8 @@ import TocTreeView from '../treeView/TocTreeView';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencilAlt, faBug, faHashtag, faLightbulb } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { Link, useI18next } from "gatsby-plugin-react-i18next";
+import * as styles from './index.module.less';
 
 const result = {
   doc: {},
@@ -27,13 +29,133 @@ const result = {
 };
 
 const Aside = (props) => {
-  const { label, href, type, locale } = props;
+  const {
+    category = "doc",
+    items,
+    title,
+    className = '',
+    locale,
+    version,
+    editPath,
+    mdTitle
+  } = props;
+  const { langugae, t } = useI18next();
+
+  // editBtn issueBtn; bugBtn; suggestBtn; joinBtn
+  const [commonEditBtn, issueBtn, bugBtn, suggestBtn, discussBtn, commonJoinBtn] = [
+    {
+      label: t(editBtn.label),
+      icon: faPencilAlt,
+    },
+    {
+      label: t(issueBtn.label),
+      icon: faBug,
+    },
+    {
+      label: t(bugBtn.label),
+      icon: faBug,
+    },
+    {
+      label: t(suggestBtn.label),
+      icon: faLightbulb,
+    },
+    {
+      label: t(discussBtn.label),
+      icon: faGithub,
+    },
+    {
+      label: t(joinBtn.label),
+      icon: faHashtag,
+    },
+
+  ];
+  const btnConfiguration = {
+    doc: ({ locale, version, editPath, mdTitle }) => {
+      const name = editPath.split('/').pop();
+      const title = `${version} ${mdTitle} (${name}) Doc Update`;
+
+      return {
+        editBtn: {
+          label: commonEditBtn.label,
+          link: `https://github.com/milvus-io/milvus-docs/edit/${version}/site/${locale === 'en' ? 'en' : 'zh-CN'
+            }/${editPath}`,
+          icon: commonEditBtn.icon,
+        },
+        issueBtn: {
+          label: issueBtn.label,
+          link: `https://github.com/milvus-io/milvus-docs/issues/new?assignees=&labels=&template=--error-report.yaml&title=${title}`,
+          icon: issueBtn.icon,
+        },
+        suggestBtn: {
+          label: suggestBtn.label,
+          link: 'https://github.com/milvus-io/milvus-docs/issues/new?assignees=&labels=&template=--new-content-proposal.yaml&title=New Doc Proposal',
+          icon: suggestBtn.icon,
+        }
+      };
+    },
+    api: ({ apiReferenceData }) => {
+      const { projName, relativePath, sourceUrl } = apiReferenceData;
+      const title = `${projName},${version},${relativePath}`;
+      return {
+        editBtn: {
+          label: commonEditBtn.label,
+          link: sourceUrl,
+          icon: commonEditBtn.icon,
+        },
+        discussBtn: {
+          label: discussBtn.label,
+          link: 'https://github.com/milvus-io/milvus/discussions/new',
+          icon: discussBtn.icon,
+        },
+        bugBtn: {
+          label: bugBtn.label,
+          link: `https://github.com/milvus-io/milvus/issues/new?assignees=&labels=&template=bug_report.md&title=${title}`,
+          icon: bugBtn.icon,
+        },
+      };
+    },
+    community: ({ locale, editPath, id }) => ({
+      editBtn: {
+        label: commonEditBtn.label,
+        link: `https://github.com/milvus-io/web-content/edit/master/community/site/${locale === 'en' ? 'en' : 'zh-CN'
+          }/${editPath}`,
+        icon: commonEditBtn.icon,
+      },
+      bugBtn: {
+        label: bugBtn.label,
+        link: 'https://github.com/milvus-io/web-content/discussions/new',
+        icon: bugBtn.icon,
+      },
+    }),
+  };
+
+  const generateBtnroup = (category, props) => {
+    const btns = btnConfiguration[category](props);
+    return (
+      <>
+        {
+          btns.map(btn => (
+            <a href={btn.link}>
+              <FontAwesomeIcon className={styles.global} icon={btn.icon} />
+              <span>{btn.label}</span>
+            </a>
+          ))
+        }
+      </>
+    );
+  };
   return (
     <section className="">
       <div className=''>
-
+        {
+          generateBtnroup(category, props)
+        }
       </div>
-      <TocTreeView {...props} />
+      <TocTreeView
+        items={items}
+        title={title}
+        className={className}
+      />
     </section>
 
   );
